c2734d191fd4645e8c8192e726865116124e098c
蔡建红 <caijianhong@cjhsMacBook-Pro.local>
2020-10-17 20:26:11 +0800

    init

---
+++ src/api/index.js
@@ -0,0 +1,8 @@
+/*
+包含n个接口请求函数的模块
+函数的返回值: promise对象
+ */
+
+import ajax from './ajax';
+
+export const reqAddress = (geohash) => ajax(`/position/${geohash}`);
---
+++ src/router/index.js
@@ -0,0 +1,74 @@
+// // import Vue from 'vue'
+// // import Router from 'vue-router'
+// // import HelloWorld from '@/components/HelloWorld'
+
+// // Vue.use(Router)
+
+// // export default new Router({
+// //   routes: [
+// //     {
+// //       path: '/',
+// //       name: 'HelloWorld',
+// //       component: HelloWorld
+// //     }
+// //   ]
+// // })
+/*
+路由器对象模块
+ */
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+// import VueAwesomeSwiper from 'vue-awesome-swiper';
+
+import Msite from './../pages/Msite/Msite';
+import Search from './../pages/Search/Search';
+import Order from './../pages/Order/Order';
+import Profile from './../pages/Profile/Profile';
+import Login from './../pages/Login/Login';
+
+// 使用插件
+Vue.use(VueRouter)
+// Vue.use(VueAwesomeSwiper)
+
+// VueRouter是一个构造函数， 里面传一个配置对象（配置路由）
+export default new VueRouter({
+  routes: [
+    {
+      path: '/msite',
+      component: Msite,
+      meta: {
+        showFooter: true
+      }
+    },
+    {
+      path: '/search',
+      component: Search,
+      meta: {
+        showFooter: true
+      }
+    },
+    {
+      path: '/order',
+      component: Order,
+      meta: {
+        showFooter: true
+      }
+    },
+    {
+      path: '/profile',
+      component: Profile,
+      meta: {
+        showFooter: true
+      }
+    },
+    {
+      path: '/',
+      redirect: '/msite'
+    },
+    {
+      path: '/login',
+      component: Login
+    },
+  ]
+})
+
